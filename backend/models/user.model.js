eb5105697136fd726ec72aaa8a042179d33decd0
Itsarun01 <hey.arunkumar05@gmail.com>
2025-03-10 23:20:06 +0530

    add user schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "recruiter"],
+      required: true,
+    },
+    profile: {
+      profilePhoto: {type: String, default: ""},
+      bio: {
+        type: String,
+      },
+      skills: [{type: String}],
+      resume: {type: String},
+      resumePathName: {type: String},
+      company: {type: mongoose.Schema.Types.ObjectId, ref: "Company"},
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);

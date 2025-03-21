5b3182ea7d42dde65faee0d034f60e0c5be54b65
hiarun01 <hiarun02@gmail.com>
2025-03-21 11:29:17 +0530

    setup company route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 
 import dotenv from "dotenv";
 dotenv.config({});
@@ -23,6 +24,10 @@ let PORT = process.env.PORT || 3000;
 // Api
 // userApis
 app.use("/api/user", userRoute);
+
+// company apis
+
+app.use("/api/company", companyRoute);
 
 app.listen(PORT, () => {
   connectDB();

663430de1a052234a57a08a48b25a977a8a1a226
hiarun01 <hiarun02@gmail.com>
2025-03-21 11:59:08 +0530

    fixed syntex error

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -5,14 +5,14 @@ import {
   getCompanyById,
   registerCompany,
   updateCompany,
-} from "../controllers/company.controller";
-import isAuthenticated from "../middlewares/isAuthenticated";
+} from "../controllers/company.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/register").post(isAuthenticated, registerCompany);
-router.route("/get").post(isAuthenticated, getCompany);
-router.route("/get/:id").post(isAuthenticated, getCompanyById);
-router.route("/update/:id").post(isAuthenticated, updateCompany);
+router.route("/get").get(isAuthenticated, getCompany);
+router.route("/get/:id").get(isAuthenticated, getCompanyById);
+router.route("/update/:id").put(isAuthenticated, updateCompany);
 
 export default router;

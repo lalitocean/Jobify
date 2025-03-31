921ae67eef90852ffd09fcfb0e860107d52a3fa0
hiarun01 <hiarun02@gmail.com>
2025-03-31 23:23:01 +0530

    jobs page cards done!

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -1,6 +1,7 @@
 import {Bookmark} from "lucide-react";
 import {Badge} from "./ui/badge";
 import {Button} from "./ui/button";
+import {Avatar, AvatarImage} from "./ui/avatar";
 
 const JobCard = ({index}) => {
   return (
@@ -9,22 +10,23 @@ const JobCard = ({index}) => {
       className=" bg-white border-2 border-gray-100 rounded-2xl shadow-1xl  cursor-pointer p-3"
     >
       <div className="flex justify-between items-center">
-        <p>2 day ago</p>
+        <Badge variant="outline">2 day ago</Badge>
         <Button variant="outline" className="rounded-full">
           <Bookmark />
         </Button>
       </div>
+
       <div className="">
-        <img
-          className="h-52 w-full"
-          src="https://st3.depositphotos.com/43745012/44906/i/450/depositphotos_449066958-stock-photo-financial-accounting-logo-financial-logo.jpg"
-          alt=""
-        />
-      </div>
-      <div className="">
-        <div className="mt-5">
-          <h2 className="font-medium text-lg">Company Name</h2>
-          <p className="text-sm text-gray-500">Loation</p>
+        <div className="mt-5 flex items-center gap-5 ">
+          <Button variant="outline" size="icon" className="w-16 h-16 my-2">
+            <Avatar>
+              <AvatarImage src="https://st3.depositphotos.com/43745012/44906/i/450/depositphotos_449066958-stock-photo-financial-accounting-logo-financial-logo.jpg"></AvatarImage>
+            </Avatar>
+          </Button>
+          <div>
+            <h2 className="font-medium text-lg">Company Name</h2>
+            <p className="text-sm text-gray-500">Loation</p>
+          </div>
         </div>
         <div className="pt-2 pb-2">
           <h1 className="text-lg font-bold">JOb title</h1>
@@ -37,6 +39,11 @@ const JobCard = ({index}) => {
           <Badge variant="ghost">Full Time</Badge>
           <Badge variant="ghost">india</Badge>
           <Badge variant="ghost">12 position</Badge>
+        </div>
+        <div className="flex  gap-5 itmes-center mt-3">
+          <Button className="shadow-2xs bg-red-600 hover:bg-red-700 font-semibold">
+            Apply
+          </Button>
         </div>
       </div>
     </div>
--- frontend/src/components/LatestJobes.jsx
+++ frontend/src/components/LatestJobes.jsx
@@ -5,16 +5,16 @@ const LatestJobOpenings = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const LatestJobes = () => {
   return (
     <>
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 lg:gap-8 h-auto mb-10">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 h-auto mb-10">
         <dir className="py-10">
           <h2 className="font-bold text-3xl lg:text-3xl md:text-2xl sm:text-2xl ">
             <strong>
               <span className="text-red-700 t">#Latest</span>
-            </strong>{" "}
+            </strong>
             Job Openings
           </h2>
         </dir>
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-3 md:grid-cols-2 ">
+        <div className="grid  gap-6 lg:grid-cols-3 md:grid-cols-2 ">
           {LatestJobOpenings.map((job, index) => (
             <LatestJobCards job={job} index={index} />
           ))}
